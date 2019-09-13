91b4e04366558ce95c58b851c42b245e27d24f33
Ellie Hansen <ellie.h580@stu.nebo.edu>
2019-09-13 10:01:05 -0600

    added create with code project

---
+++ createwithcode/prototype1/Assets/scripts/playercontroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float speed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // oh yeah woohoo
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
