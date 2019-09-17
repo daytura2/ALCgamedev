41bde5a64222077ac05f9eb0b0a1efa4b4383869
Ellie Hansen <ellie.h580@stu.nebo.edu>
2019-09-17 10:26:54 -0600

    turn speed and cleanup

--- createwithcode/prototype1/Assets/scripts/playercontroller.cs
+++ createwithcode/prototype1/Assets/scripts/playercontroller.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class playercontroller : MonoBehaviour
 {
-    public float speed = 20f;
+    private float speed = 20f;
+    private float turnSpeed = 45f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,11 @@ public class playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // oh yeah woohoo
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
