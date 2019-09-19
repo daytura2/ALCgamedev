dc5da9175be266c37d6676f4b4807231ca635fcf
Ellie Hansen <ellie.h580@stu.nebo.edu>
2019-09-19 10:29:06 -0600

    plane

---
+++ createwithcode/prototype1/Assets/Challenge 1/Scripts/Spin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float rotationSpeed = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed;
+    }
+}
