cfe08209f7256708c934383574c74bc35117528f
Ellie Hansen <ellie.h580@stu.nebo.edu>
2019-10-21 10:09:37 -0600

    a

--- createwithcode/New Unity Project/Assets/scripts/DestroyOutOfBounds.cs
+++ createwithcode/New Unity Project/Assets/scripts/DestroyOutOfBounds.cs
@@ -12,10 +12,15 @@ public class DestroyOutOfBounds : MonoBehaviour
 
     // Update is called once per frame
     private float topBound = 30;
+    private float lowerBound = -10;
 
     void Update()
     {
         if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
         {
             Destroy(gameObject);
         }
