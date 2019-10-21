ae606ebb98ab47be6a823e436fba0b0bed39433b
Ellie Hansen <ellie.h580@stu.nebo.edu>
2019-10-21 09:22:11 -0600

    project 2

---
+++ createwithcode/New Unity Project/Assets/scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private float topBound = 30;
+
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
