9b0b4afecb74a11f5c81847423dc3d18c7883d0d
Ellie Hansen <ellie.h580@stu.nebo.edu>
2019-11-06 10:27:37 -0700

    jump

--- createwithcode/New Unity Project/Assets/scripts/SpawnManager.cs
+++ createwithcode/New Unity Project/Assets/scripts/SpawnManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+  
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +13,16 @@ public class SpawnManager : MonoBehaviour
     }
 
     // Update is called once per frame
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX)), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos,
+            animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
---
+++ createwithcode/prototype3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
