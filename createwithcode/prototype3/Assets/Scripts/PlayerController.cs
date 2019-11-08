aa2d1c3ad57beafaa9f04abcdf50be40eaa6f8d0
Ellie Hansen <ellie.h580@stu.nebo.edu>
2019-11-08 10:29:32 -0700

    spawns obstacles

--- createwithcode/prototype3/Assets/Scripts/PlayerController.cs
+++ createwithcode/prototype3/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
+    public bool isOnGround = true;
 
     void Start()
     {
@@ -18,9 +19,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
