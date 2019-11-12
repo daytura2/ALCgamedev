04c40dde8b749af972fcb85d5876c44ea4c467c3
Ellie Hansen <ellie.h580@stu.nebo.edu>
2019-11-12 10:25:57 -0700

    scrolling bg, game over

--- createwithcode/prototype3/Assets/Scripts/MoveLeft.cs
+++ createwithcode/prototype3/Assets/Scripts/MoveLeft.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
+    private float speed = 30;
+    private PlayerController playerControllerScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
-    private float speed = 30;
+    
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        
     }
 }
--- createwithcode/prototype3/Assets/Scripts/PlayerController.cs
+++ createwithcode/prototype3/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
 
     void Start()
     {
@@ -28,6 +29,13 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over");
+        }
     }
 }
